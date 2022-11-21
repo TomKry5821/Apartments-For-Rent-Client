362fd70b25f359f78d5135615f119ae1ee04c3b6
TomKry5821 <tomakry729@student.polsl.pl>
2022-11-21 23:48:17 +0100

    Added login page

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable no-shadow */
+import { React } from "react";
+import { Grid } from "@mui/material";
+import Filter from "../coponents/Filter";
+import Offers from "../coponents/Offers";
+import NavbarLogin from "../coponents/NavbarLogin";
+
+const LoginPage = function () {
+  return (
+    <div className="main-container">
+      <div className="navStrip">
+        <div className="navText">
+          <NavbarLogin />
+        </div>
+      </div>
+      <div className="searchBG">
+        <div className="searchBar">
+          <Grid container direction="row" justifyContent="space-around">
+            <Grid item>
+              <Filter />
+            </Grid>
+            <Grid item alignContent="baseline">
+              <Offers />
+              <Offers />
+              <Offers />
+              <Offers />
+            </Grid>
+          </Grid>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
