50bdefc8e5dd7e5215a9cc07d7900c24049a5cfc
TomKry5821 <tomakry729@student.polsl.pl>
2022-11-21 23:44:29 +0100

    Added footer

---
+++ src/coponents/Footer.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable func-names */
+const Footer = function () {
+  return (
+    <footer className="footer">
+      <p> Copyright &copy; 2022 </p>
+    </footer>
+  );
+};
+
+export default Footer;
