4abbc18f965d653b1b4b88afedf5719a72be3e03
TomKry5821 <tomakry729@student.polsl.pl>
2022-11-21 23:48:06 +0100

    Added main page for logged user

---
+++ src/pages/LoggedMainPage.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable no-shadow */
+import { React } from "react";
+import { Grid } from "@mui/material";
+import Filter from "../coponents/Filter";
+import OffersLogged from "../coponents/OffersLogged";
+import NavbarLogged from "../coponents/NavbarLogged";
+
+const LoggedMainPage = function () {
+  return (
+    <div className="main-container">
+      <div className="navStrip">
+        <div className="navText">
+          <NavbarLogged />
+        </div>
+      </div>
+      <div className="searchBG">
+        <div className="searchBar">
+          <Grid container direction="row" justifyContent="space-around">
+            <Grid item>
+              <Filter />
+            </Grid>
+            <Grid item alignContent="baseline">
+              <OffersLogged />
+              <OffersLogged />
+              <OffersLogged />
+              <OffersLogged />
+            </Grid>
+          </Grid>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoggedMainPage;
