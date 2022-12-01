0a90d72e81b6ae789231d81ff0b9385a88abda03
TomKry5821 <tomakry729@student.polsl.pl>
2022-12-01 21:58:42 +0100

    remved unused import

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { UserProvider } from "./context/UserContext";
 
 ReactDOM.render(
   <React.StrictMode>
