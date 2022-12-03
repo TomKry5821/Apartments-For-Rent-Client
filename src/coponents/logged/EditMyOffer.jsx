c1924b6f8084ecf848f762b8d0ea7956bfe22d56
TomKry5821 <tomakry729@student.polsl.pl>
2022-12-03 23:50:40 +0100

    Added announcement editing logic

--- src/coponents/logged/EditMyOffer.jsx
+++ src/coponents/logged/EditMyOffer.jsx
@@ -8,54 +8,241 @@ import {
   CardHeader,
   Grid,
   TextField,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Paper,
+  Input
 } from "@mui/material";
 import * as React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { useNavigate } from "react-router-dom";
-import bedroom from "../../images/bedroom.jpeg";
-import garage from "../../images/garage.jpeg";
-import kitchen from "../../images/kitchen.jpeg";
-import livingRoom from "../../images/livingRoom.jpeg";
-import view from "../../images/view.jpeg";
-import EditOfferDetails from "./EditOfferDetails";
+import { useState, useRef } from "react";
 
 const EditMyOffer = function () {
   const navigate = useNavigate();
 
+  const userId = localStorage.getItem("userId");
+  const announcementId = localStorage.getItem("announcementId");
+  const mainPhoto = localStorage.getItem("mainPhoto");
+  const photos = JSON.parse(localStorage.getItem("photos"));
+  const title = localStorage.getItem("title");
+  const city = localStorage.getItem("city");
+  const district = localStorage.getItem("district");
+  const zipCode = localStorage.getItem("zipCode");
+  const street = localStorage.getItem("street");
+  const buildingNumber = localStorage.getItem("buildingNumber");
+  const localNumber = localStorage.getItem("localNumber");
+  const roomsNumber = localStorage.getItem("roomsNumber");
+  const rentalAmount = localStorage.getItem("rentalAmount");
+  const rentalTerm = localStorage.getItem("rentalTerm");
+  const caution = localStorage.getItem("caution");
+  const content = localStorage.getItem("content");
+
+  const newMainPhoto = useState(null);
+  const newPhotos = useState([]);
+  const newTitle = useRef("");
+  const newCity = useRef("");
+  const newDistrict = useRef("");
+  const newZipCode = useRef("");
+  const newStreet = useRef("");
+  const newBuildingNumber = useRef("");
+  const newLocalNumber = useRef(0);
+  const newRoomsNumber = useRef(0);
+  const newRentalAmount = useRef(0);
+  const newRentalTerm = useRef("");
+  const newCaution = useRef(0);
+  const newContent = useRef("");
+
+  const changeAnnouncementDetails = function () {
+    const URL = "http://localhost:8010";
+    const userId = localStorage.getItem("userId");
+    const authorizationToken = localStorage.getItem("authorizationToken");
+    const data = new FormData();
+    //for (const item of newPhotos) {
+    // data.append('photos', item);
+    //}
+    data.append('title', newTitle.current.value);
+    //data.append('mainPhoto', newMainPhoto);
+    data.append('userId', userId);
+    data.append('roomsNumber', newRoomsNumber.current.value);
+    data.append('rentalTerm', newRentalTerm.current.value);
+    data.append('caution', newCaution.current.value);
+    data.append('rentalAmount', newRentalAmount.current.value);
+    data.append('content', newContent.current.value);
+    data.append('district', newDistrict.current.value);
+    data.append('city', newCity.current.value);
+    data.append('zipCode', newZipCode.current.value);
+    data.append('street', newStreet.current.value);
+    data.append('buildingNumber', newBuildingNumber.current.value);
+    data.append('localNumber', newLocalNumber.current.value);
+    fetch(URL + "/announcement/api/v1/announcements/" + announcementId, {
+      method: "PUT",
+      body: data,
+      headers: {
+        'Authorization': authorizationToken
+      }
+    }).then((response) => response.json())
+      .then((data) => {
+        if (data.status > 299) {
+          alert("Coś poszło nie tak, sprawdź uzupełnione pola i spróbuj jeszcze raz");
+        } else {
+          alert("Pomyślnie zmieniono ogłoszenie");
+          navigate("/logged/profile/offer");
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        alert("Coś poszło nie tak, sprawdź uzupełnione pola i spróbuj jeszcze raz");
+      });
+  }
+
   return (
     <Grid container justifyContent="space-between">
       <Grid item>
-        <Carousel width="600px">
-          <div>
-            <img src={kitchen} alt="" />
-          </div>
-          <div>
-            <img src={bedroom} alt="" />
-          </div>
-          <div>
-            <img src={livingRoom} alt="" />
-          </div>
-          <div>
-            <img src={view} alt="" />
-          </div>
-          <div>
-            <img src={garage} alt="" />
-          </div>
-        </Carousel>
+        {
+          <Grid item>
+            <Carousel width="600px">
+              {
+                < div >
+                  <img src={"data:image/png;base64," + mainPhoto} />
+                </div>
+              }
+              {
+                photos.map((photo) => (
+                  <div>
+                    <img src={"data:image/png;base64," + photo} />
+                  </div>
+                ))
+              }
+            </Carousel>
+          </Grid>
+        }
       </Grid>
       <Grid item>
         <Grid container direction="column" spacing={3}>
           <Grid item>
             <Card sx={{ width: "600px" }}>
-              <CardHeader subheader="14 Wrzesień 2022">
-                <TextField defaultValue="Mieszkanie 3 pokojowe - centrum Gliwic" />
-              </CardHeader>
-
               <CardContent>
                 <Grid container direction="column" spacing={3}>
                   <Grid item>
-                    <EditOfferDetails />
+                    <TableContainer component={Paper}>
+                      <Table sx={{ minWidth: 250 }} aria-label="simple table">
+                        <TableRow key="title">
+                          <TableCell scope="row"> Tytuł </TableCell>
+                          <TableCell align="right">
+                            <TextField
+                              align="right"
+                              defaultValue={title}
+                              inputRef={newTitle}
+                            />
+                          </TableCell>
+                        </TableRow>
+                        <TableRow key="district">
+                          <TableCell scope="row"> Województwo </TableCell>
+                          <TableCell align="right">
+                            <TextField
+                              align="right"
+                              defaultValue={district}
+                              inputRef={newDistrict}
+                            />
+                          </TableCell>
+                        </TableRow>
+                        <TableRow key="city">
+                          <TableCell scope="row">Miasto</TableCell>
+                          <TableCell align="right">
+                            <TextField
+                              align="right"
+                              defaultValue={city}
+                              inputRef={newCity} />
+                          </TableCell>
+                        </TableRow>
+                        <TableRow key="zipCode">
+                          <TableCell scope="row">Kod pocztowy</TableCell>
+                          <TableCell align="right">
+                            <TextField
+                              align="right"
+                              defaultValue={zipCode}
+                              inputRef={newZipCode}
+                            />
+                          </TableCell>
+                        </TableRow>
+                        <TableRow key="street">
+                          <TableCell scope="row">Ulica</TableCell>
+                          <TableCell align="right">
+                            <TextField
+                              align="right"
+                              defaultValue={street}
+                              inputRef={newStreet}
+                            />
+                          </TableCell>
+                        </TableRow>
+                        <TableRow key="buildingNumber" >
+                          <TableCell scope="row">Numer budynku</TableCell>
+                          <TableCell align="right">
+                            <TextField
+                              align="right"
+                              defaultValue={buildingNumber}
+                              inputRef={newBuildingNumber}
+                            />
+                          </TableCell>
+                        </TableRow>
+                        <TableRow key="localNumber">
+                          <TableCell scope="row">Numer lokalu</TableCell>
+                          <TableCell align="right">
+                            <TextField
+                              align="right"
+                              defaultValue={localNumber}
+                              inputRef={newLocalNumber}
+                            />
+                          </TableCell>
+                        </TableRow>
+                        <TableRow key="roomsNumber" >
+                          <TableCell scope="row">Ilość pokoi</TableCell>
+                          <TableCell align="right">
+                            <TextField
+                              align="right"
+                              defaultValue={roomsNumber}
+                              inputRef={newRoomsNumber}
+                            />
+                          </TableCell>
+                        </TableRow>
+                        <TableRow key="rentalAmount">
+                          <TableCell scope="row">Opłata miesięczna</TableCell>
+                          <TableCell align="right">
+                            <TextField
+                              align="right"
+                              defaultValue={rentalAmount} zł
+                              inputRef={newRentalAmount}
+                            />
+                          </TableCell>
+                        </TableRow>
+                        <TableRow key="caution" >
+                          <TableCell scope="row">Wysokość kaucji</TableCell>
+                          <TableCell align="right">
+                            <TextField
+                              align="right"
+                              defaultValue={caution} zł
+                              inputRef={newCaution}
+                            />
+                          </TableCell>
+                        </TableRow>
+                        <TableRow key="rentalTerm">
+                          <TableCell scope="row">Od kiedy można wynająć</TableCell>
+                          <TableCell align="right">
+                            <Input
+                              type="date"
+                              align="right"
+                              defaultValue={rentalTerm}
+                              inputRef={newRentalTerm}
+                            />
+                          </TableCell>
+                        </TableRow>
+                      </Table>
+                    </TableContainer>
                   </Grid>
                   <Grid item>
                     <TextField
@@ -63,11 +250,8 @@ const EditMyOffer = function () {
                       multiline
                       maxRows={20}
                       fullWidth
-                      defaultValue="Sprzedam mieszkanie 3 pokojowe w spokojnej okolicy,
-                      bezczynszowe. Miejsce garażowe. Teren ogrodzony z pięknym
-                      widokiem. Blisko centrum. Mieszkanie bardzo jasne i
-                      czyste. Dostępne od zaraz. Więcej informacji w wiadomości
-                      prywatnej."
+                      defaultValue={content}
+                      inputRef={newContent}
                     />
                   </Grid>
                 </Grid>
@@ -83,7 +267,7 @@ const EditMyOffer = function () {
                 fontFamily: "Titillium Web, sans-serif;",
               }}
               onClick={() => {
-                navigate("/logged/profile/offer");
+                changeAnnouncementDetails();
               }}
             >
               zapisz
